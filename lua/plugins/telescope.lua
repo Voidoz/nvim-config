67f9440cb37bbe71c9aaa67193ff67458df2d5df
Voidoz <41652531+Voidoz@users.noreply.github.com>
2024-10-03 18:54:41 +1000

    Update telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,18 +2,18 @@ local lead = '<leader>f'
 
 return {
     'nvim-telescope/telescope.nvim',
+    lazy = false,
     dependencies = {
         'nvim-lua/plenary.nvim',
-        'sharkdp/fd',
         'nvim-treesitter/nvim-treesitter',
         'nvim-tree/nvim-web-devicons',
     },
     keys = {
         { lead,                                         desc = 'Telescope' },
-        { lead .. '.', '<cmd>Telescope<CR>',            desc = 'All' },
+        { lead .. 't', '<cmd>Telescope<CR>',            desc = 'All' },
         { lead .. 'f', '<cmd>Telescope find_files<CR>', desc = 'Files' },
         { lead .. 'd', '<cmd>Telescope fd<CR>',         desc = 'Directories' },
-        { lead .. 't', '<cmd>Telescope live_grep<CR>',  desc = 'Text' },
+        { lead .. 'g', '<cmd>Telescope live_grep<CR>',  desc = 'Text' },
         { lead .. 'b', '<cmd>Telescope buffers<CR>',    desc = 'Buffers'},
     },
 }
