c783233d3269bc9267250e58dde5aaca0c0f3091
Voidoz <41652531+Voidoz@users.noreply.github.com>
2026-06-12 08:33:30 +1000

    Move notifications to dedicated file

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,5 @@
 return {
     {
-        'rcarriga/nvim-notify',
-        enabled = require('config.vscode.enabled').never,
-        version = '^3',
-    },
-    {
         'stevearc/dressing.nvim',
         enabled = require('config.vscode.enabled').never,
         init = function()
---
+++ lua/plugins/ui/notifications.lua
@@ -0,0 +1,5 @@
+return {
+	'rcarriga/nvim-notify',
+	enabled = require('config.vscode.enabled').never,
+	version = '^3',
+};
